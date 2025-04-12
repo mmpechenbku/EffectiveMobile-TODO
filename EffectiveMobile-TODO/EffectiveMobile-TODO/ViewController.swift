6997c5ee9d7e89fbc2a106c7bdc3d1061ba3fa37
mm pechenbku <mmpechenbku@gmail.com>
2025-04-12 20:54:56 +0300

    initial commit

---
+++ EffectiveMobile-TODO/EffectiveMobile-TODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EffectiveMobile-TODO
+//
+//  Created by mm pechenbku on 12.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
